98ddbed3c39cee485b06bf1f7728e2d3c95d7095
zhangdi <1258956799@qq.com>
2022-01-02 17:27:58 +0800

    first commit

--- src/05_usecallback使用/01_usecallback不能进行性能优化.js
+++ src/05_usecallback使用/01_usecallback不能进行性能优化.js
@@ -1,7 +1,16 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, memo } from 'react';
+
+// useCallback在将一个组件中的函数 传递给子元素进行回调使用时 使用useCallback对函数进行处理
+
+const HYButton = memo((props) => {
+  console.log('HYButton重新渲染:' + props.title);
+  return <button onClick={props.increment}>HYBuuton+1</button>;
+});
 
 export default function CllbackHookDemo01() {
+  console.log('CllbackHookDemo01重新渲染');
   const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
 
   const increment1 = () => {
     setCount(count + 1);
@@ -15,9 +24,19 @@ export default function CllbackHookDemo01() {
   }, [count]);
   return (
     <div>
-      <h2>{count}</h2>
-      <button onClick={increment1}>+1</button>
-      <button onClick={increment2}>+1</button>
+      <h2>CllbackHookDemo01:{count}</h2>
+      {/* <button onClick={increment1}>+1</button>
+      <button onClick={increment2}>+1</button> */}
+      <HYButton increment={increment1} title="Btn1"></HYButton>
+      <HYButton increment={increment2} title="Btn2"></HYButton>
+
+      <button
+        onClick={(e) => {
+          setShow(!show);
+        }}
+      >
+        show切换
+      </button>
     </div>
   );
 }
